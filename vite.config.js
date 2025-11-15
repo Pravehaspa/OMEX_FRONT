23e09319a7ec57075a699f10f90d930b41156beb
Gupta-02 <Gupta-02@users.noreply.github.com>
2025-11-16 02:20:00 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
   envDir: './',
   envPrefix: 'VITE_',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
   define: {
     'process.env.VITE_BACKEND_URL': '"https://backend-o213.onrender.com/"'
   }
