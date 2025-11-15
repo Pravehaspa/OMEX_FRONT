6eb5df6c9740deb95bd904088198d8a964e32ce7
Gupta-02 <Gupta-02@users.noreply.github.com>
2025-11-16 02:18:27 +0530

    Add AuroraText component

---
+++ src/components/AuroraText.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/AuroraText.css';
+
+const AuroraText = ({ children, className = '' }) => {
+  return (
+    <span className={`aurora-text ${className}`}>
+      {children}
+    </span>
+  );
+};
+
+export default AuroraText;
